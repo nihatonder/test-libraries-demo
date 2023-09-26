915d04943a7d90c4291d4d421f76f7ac5e58e824
Nihat Onder <nihatonder87@gmail.com>
2023-09-26 10:17:51 +0300

    Updated examples

--- src/test/java/com/nonder/example/TestcontainersTest.java
+++ src/test/java/com/nonder/example/TestcontainersTest.java
@@ -1,9 +1,15 @@
 package com.nonder.example;
 
-
-import jakarta.annotation.PostConstruct;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.GenericContainer;
@@ -13,24 +19,41 @@ import org.testcontainers.junit.jupiter.Container;
 @SpringBootTest
 @org.testcontainers.junit.jupiter.Testcontainers
 public class TestcontainersTest {
+    private CachingConnectionFactory connectionFactory;
 
     @Container
     private GenericContainer rabbitMQContainer = new GenericContainer("rabbitmq:3-management")
             .withExposedPorts(5672);  // Port inside the container
 
-    @PostConstruct
-    public void initContainer() {
+    @BeforeEach
+    public void init() {
         rabbitMQContainer.start();
+
+        String address = rabbitMQContainer.getHost();
+        Integer port = rabbitMQContainer.getFirstMappedPort();
+
+        connectionFactory = new CachingConnectionFactory(address, port);
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+
+        // Declare exchange and queue
+        TopicExchange testExchange = new TopicExchange("test-exchange");
+        Queue testQueue = new Queue("test-queue");
+        Binding binding = BindingBuilder.bind(testQueue).to(testExchange).with("test-routing-key");
+
+        rabbitAdmin.declareExchange(testExchange);
+        rabbitAdmin.declareQueue(testQueue);
+        rabbitAdmin.declareBinding(binding);
     }
 
     @Test
     void testSendMessageToRabbitMQ() {
-        String address = rabbitMQContainer.getHost();
-        Integer port = rabbitMQContainer.getFirstMappedPort();  // Random port outside the container
-
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(address, port);
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
 
+        // Send a message
         rabbitTemplate.convertAndSend("test-exchange", "test-routing-key", "Test Message");
+
+        // Receive and verify the message
+        Object receivedMessage = rabbitTemplate.receiveAndConvert("test-queue");
+        Assertions.assertEquals("Test Message", receivedMessage);
     }
 }
